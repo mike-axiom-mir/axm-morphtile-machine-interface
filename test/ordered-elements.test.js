e28c9a380d6425cb6611d7a74903bf8e89dd0c62
mike-axiom-mir <miketobi90@gmail.com>
2026-09-21 03:25:08 +0200

    test: align ordered-elements version assertion

--- test/ordered-elements.test.js
+++ test/ordered-elements.test.js
@@ -26,7 +26,7 @@ test("ordered elements compile into one atomic view.set without copied state", (
   });
 
   assert.equal(out.status, "CANDIDATE");
-  assert.equal(out.machine.version, "0.5.7");
+  assert.equal(out.machine.version, "0.5.8");
   assert.deepEqual(out.candidate.operation, {
     op: "view.set",
     id: "mt_tower",
